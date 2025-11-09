fda0e85a878af71b5829e665a44f1d42a8d46291
opuadm <opuadm@gmail.com>
2025-11-09 20:19:56 +0100

    Didn't fail that much this time

--- src/main/java/com/opuadm/linuxifymc/commands/cli/cmds/Neofetch.java
+++ src/main/java/com/opuadm/linuxifymc/commands/cli/cmds/Neofetch.java
@@ -13,21 +13,22 @@ import net.kyori.adventure.key.Key;
 public class Neofetch {
     public boolean execute(CommandSender sender, Player player, FakeFS fs, String[] args) {
         String[] art = new String[] {
-                "           +++==           ",
+                "           +++==            ",
                 "        +====++++++=        ",
                 "    ++++++++#@@#*++++++==   ",
                 "  **+++++++##%#%@+++++++*## ",
-                "  *#****++=*=--*@+==+##%%%@ ",
-                "  ##%#%##**#-=::#@%%##%%%%% ",
-                "  *####%#%#.....-%@@%%%%%%% ",
-                "  ######%#:......:@@@%%%%%% ",
-                "  ####%%*+:......:@@%%%%%%@ ",
-                "  ####+---+%....--=--*%%%%% ",
-                "  ####*==--=***%%==+*#%%%%@ ",
+                "  *#****++=*=##*@+==+##%%%@ ",
+                "  ##%#%##**#====#@%%##%%%%% ",
+                "  *####%#%#~~~~~=%@@%%%%%%% ",
+                "  ######%#=~~~~~~=@@@%%%%%% ",
+                "  ####%%*+=~~~~~~=@@%%%%%%@ ",
+                "  ####+####+%~~~~=##=*%%%%% ",
+                "  ####*==##=***%%==+*#%%%%@ ",
                 "    ##########%%%%%%%%%%    ",
                 "         #####%%%%%%        ",
                 "            #%%%            "
         };
+
         String[] info = new String[] {
                 "LinuxifyMC " + LinuxifyMC.version,
                 LinuxifyMC.kernelname + " " + LinuxifyMC.kernelver,
@@ -35,47 +36,32 @@ public class Neofetch {
                 "Current Player: " + player.getName()
         };
 
-        int artColumnWidth = 0;
-        for (String s : art) if (s.length() > artColumnWidth) artColumnWidth = s.length();
-
-        int infoMaxLen = 0;
-        for (String s : info) if (s.length() > infoMaxLen) infoMaxLen = s.length();
-
-        int fixedTotalWidth = 80;
-        if (fixedTotalWidth < infoMaxLen + 1) fixedTotalWidth = infoMaxLen + 1;
+        int chatWidthPixels = 320;
+        int uniformCharWidth = 6;
+        int defaultCharWidth = 6;
+        int artWidth = 28;
 
         boolean isPlayer = sender instanceof Player;
         Key uniform = Key.key("minecraft", "uniform");
 
-        final String FIGURE_SPACE = "\u2007";
-
         for (int i = 0; i < art.length; i++) {
             String artLine = art[i];
-            artLine = artLine.replaceAll("[\\r\\n]+$", "");
-
             String infoLine = i < info.length ? info[i] : "";
 
-            int infoLen = infoLine.length();
-            int infoStart = fixedTotalWidth - infoLen;
-
-            if (infoStart < 1) infoStart = 1;
-
-            String artForOutput = artLine;
-            if (artForOutput.length() > infoStart - 1) {
-                artForOutput = artForOutput.substring(0, Math.max(0, infoStart - 1));
-            }
-
-            int padCount = infoStart - artForOutput.length();
-            if (padCount < 1) padCount = 1;
-
             if (isPlayer) {
-                String spacer = FIGURE_SPACE.repeat(padCount);
-                Component artComp = Component.text(artForOutput).font(uniform);
-                Component infoComp = Component.text(spacer + infoLine);
-                player.sendMessage(artComp.append(infoComp));
+                int artPixels = artWidth * uniformCharWidth;
+                int infoPixels = infoLine.length() * defaultCharWidth;
+                int usedPixels = artPixels + infoPixels;
+                int remainingPixels = chatWidthPixels - usedPixels;
+                int paddingChars = Math.max(0, remainingPixels / uniformCharWidth);
+
+                Component artComp = Component.text(artLine).font(uniform);
+                Component spacerComp = Component.text(" ".repeat(paddingChars)).font(uniform);
+                Component infoComp = Component.text(infoLine);
+
+                player.sendMessage(artComp.append(spacerComp).append(infoComp));
             } else {
-                String spacer = " ".repeat(padCount);
-                sender.sendMessage(artForOutput + spacer + infoLine);
+                sender.sendMessage(artLine + " ".repeat(Math.max(0, 53 - artWidth - infoLine.length())) + infoLine);
             }
         }
 
